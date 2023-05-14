73311b61337ad2d969d702da1317c846ea3ee579
Giulio <giuliomerlo00@gmail.com>
2023-05-14 10:38:14 +0200

    Siamo a un passo dalla magia

--- lib/get_dataloaders.py
+++ lib/get_dataloaders.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 
+subprocess.check_call([sys.executable, 'bash', 'import_dataset.sh'])
 # implement pip as a subprocess:
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
 'pyvww'])
