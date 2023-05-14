0b45003820b5849a5353f32882bd26b94e4b3d29
Giulio <giuliomerlo00@gmail.com>
2023-05-14 10:28:49 +0200

    edit

--- lib/import_dataset.py
+++
@@ -1,5 +0,0 @@
-import os
-def unzip_dataset():
-    os.system("cd /content/drive/MyDrive/")
-    os.system("unzip -q all2014.zip -d /content")
-    os.system("cd /content")
---
+++ lib/import_dataset.sh
@@ -0,0 +1,3 @@
+cd /content/drive/MyDrive
+unzip -q all2014.zip -d /content
+cd /content/NAS4TinyML/lib
